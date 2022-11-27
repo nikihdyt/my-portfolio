aa7c86f8876f24055956eabcc4e42c4716f8e9ce
nikihdyt <nikihidayati20@gmail.com>
2022-11-27 12:28:59 +0700

    migration

--- database/migrations/2022_11_27_051747_add_picture_to_projects.php
+++ database/migrations/2022_11_27_051747_add_picture_to_projects.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            $table->string('picture');
         });
     }
 
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            $table->dropColumn('picture');
         });
     }
 };
